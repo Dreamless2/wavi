17cb7114159f7b03d523c556a3155326f304fec4
Tiago Pereira <tiagonet50@gmail.com>
2026-07-06 00:04:23 -0300

    index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => res.send('Started!'))
 app.listen(PORT, () => console.log(`Serving on port ${PORT}`))
 
 const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_ANON_KEY
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 const BUCKET_NAME = 'auth'
