ecd171b425e880e008c67522246e9a07695d29fc
Tiago Pereira <tiagonet50@gmail.com>
2026-07-12 21:21:50 -0300

    bypass.js

--- bypass.js
+++ bypass.js
@@ -9,7 +9,7 @@ import express from 'express'
 const app = express()
 const PORT = process.env.PORT || 8000
 app.get('/', (req, res) => {
-    res.send('Running')
+    res.send('Running the server')
 })
 app.listen(PORT, () => {
     console.log(`Express server running on port ${PORT}`)
